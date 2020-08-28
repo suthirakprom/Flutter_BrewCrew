b2be4d1ddd82f96a6a955d5ebc56742d653fdd81
suthirakprom <suthirakprom81@gmail.com>
2020-08-28 17:47:50 +0700

    almost done

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -52,6 +52,19 @@ class _SettingsFormState extends State<SettingsForm> {
               });
             },
           ),
+          Slider(
+            value: (_currentStrength ?? 100).toDouble(),
+            activeColor: Colors.brown[_currentStrength ?? 100],
+            inactiveColor: Colors.brown[_currentStrength ?? 100],
+            min: 100,
+            max: 900,
+            divisions: 8,
+            onChanged: (val) {
+              setState(() {
+                _currentStrength = val.round();
+              });
+            },
+          ),
           //slider
           RaisedButton(
             color: Colors.pink[400],
